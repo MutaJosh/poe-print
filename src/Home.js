1d5f30280b68ac2afb81eae2570e76cee83bfd49
carapai <olupotcharles@gmail.com>
2020-04-11 22:34:37 +0300

    Added filtering and sorting to list

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React, {useEffect} from 'react';
-import {Card, Table} from "antd";
+import {Card, Input, Table} from "antd";
 import {useStore} from "./context/context";
 import {observer} from "mobx-react";
 import {useHistory} from 'react-router-dom'
+
+const {Search} = Input;
+
 
 export const Home = observer(() => {
   const store = useStore();
@@ -14,7 +17,15 @@ export const Home = observer(() => {
 
   return <div style={{padding: 10, background: '#F7F7F7', 'minHeight': '95vh'}}>
     <Card title="Registered and allowed travellers into Uganda - COVID19 Response">
+      <Search
+        size="large"
+        placeholder="input search text"
+        allowClear={true}
+        onChange={store.onSearch}
+        style={{width: '100%', marginBottom: 20}}
+      />
       <Table
+        rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' :  'table-row-dark'}
         onRow={(record, rowIndex) => {
           return {
             onClick: event => {
@@ -26,6 +37,14 @@ export const Home = observer(() => {
         columns={store.columns}
         dataSource={store.trackedEntityInstances.rows}
         rowKey={(row) => row[0]}
+        onChange={store.handleChange}
+        pagination={{
+          showSizeChanger: true,
+          total: store.total,
+          pageSize: store.pageSize,
+          showQuickJumper: true,
+          pageSizeOptions: ['5', '10', '15', '20', '25', '50', '100']
+        }}
       />
     </Card>
   </div>
--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,4 @@
 import {decorate, observable, action, computed} from "mobx";
-import {Link} from "react-router-dom";
 import React from "react";
 import {fromPairs} from 'lodash';
 
@@ -8,7 +7,9 @@ export class Store {
   trackedEntityInstances = {rows: [], headers: []};
   query = '';
   page = 1;
-  pageSize = 15;
+  total = 0;
+  pageSize = 10;
+  sorter = 'created:desc';
   loading = false;
   currentRow;
 
@@ -19,11 +20,31 @@ export class Store {
   queryData = async () => {
     this.loading = true;
     const {trackedEntityInstances} = await this.engine.query(this.currentQuery);
+    const {metaData: {pager}} = trackedEntityInstances;
+    this.pageSize = pager.pageSize;
+    this.total = pager.total;
+    this.page = pager.page;
     this.trackedEntityInstances = trackedEntityInstances;
     this.loading = false;
   };
 
   setCurrentRow = val => this.currentRow = val;
+
+  onSearch = async (e) => {
+    this.page = 1;
+    this.query = e.target.value;
+    await this.queryData();
+  }
+
+  handleChange = async (pagination, filters, sorter) => {
+    console.log(sorter);
+    const order = sorter.field && sorter.order ? `${sorter.field}:${sorter.order === 'ascend' ? 'asc' : 'desc'}` : 'created:desc';
+    const page = pagination.pageSize !== this.pageSize || order !== this.sorter ? 1 : pagination.current;
+    this.sorter = order;
+    this.page = page;
+    this.pageSize = pagination.pageSize
+    await this.queryData();
+  }
 
   get currentQuery() {
     let params = {
@@ -31,7 +52,8 @@ export class Store {
       totalPages: 'true',
       ouMode: 'ALL',
       program: 'nBWFG3fYC8N',
-      pageSize: this.pageSize
+      pageSize: this.pageSize,
+      order: this.sorter
     };
     if (this.query !== '') {
       params = {...params, query: `LIKE:${this.query}`}
@@ -45,12 +67,13 @@ export class Store {
   }
 
   get columns() {
-    const attributes = ["instance", "CLzIR1Ye97b", "sB1IHYu2xQT", "XvETY1aTxuB", "UJiu0P8GvHt", "g4LJbkM0R24", "FZzQbW8AWVd", "oUqWGeHjj5C"];
+    const attributes = ["CLzIR1Ye97b", "sB1IHYu2xQT", "XvETY1aTxuB", "UJiu0P8GvHt", "g4LJbkM0R24", "FZzQbW8AWVd", "oUqWGeHjj5C"];
     return this.trackedEntityInstances.headers.map((a, i) => {
       return {
         key: a.name,
         title: a.column,
         dataIndex: a.name,
+        sorter: true,
         render: (text, row) => {
           return <div>{row[i]}</div>
         }
@@ -77,8 +100,11 @@ decorate(Store, {
   page: observable,
   pageSize: observable,
   currentRow: observable,
+  total: observable,
 
   queryData: action,
+  handleChange: action,
+  onSearch: action,
 
   currentQuery: computed,
   currentInstance: computed,
